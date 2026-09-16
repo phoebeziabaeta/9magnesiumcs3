d368123b10b93c9340dccf4a0d59d91b535693b6
pzvbaeta <pzvbaeta@brc.pshs.edu.ph>
2026-09-16 13:05:00 +0800

    Update classImplementation.py

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -1,6 +1,25 @@
 class attendancesheet:
-  def __init__(self, value1, value2, value3, value4):
-    self.names = value1
-    self.date = value2
-    self.present = value3
-    self.excused = value4
+    members = []
+    pres = []
+
+    def __init__(self, names, date, present, excused):
+        self.names = names
+        self.date = date
+        self.__present = max(0, present)
+        self.excused = excused
+
+    def list_members(self):
+        print("\n".join(self.names))
+        return self.names
+
+    def list_present(self):
+        print(f"Amount of members present: {len(self.pres)}")
+        return self.pres
+
+
+memb = input("Enter list of members (separate by spaces): ")
+members = memb.split()
+
+memb2 = input("Enter list of members present separate by spaces: ")
+pres = memb2.split()
+
